fd206160638267757d25956f34523707f944020d
MiguelUrena <78118827+MiguelUrena@users.noreply.github.com>
2021-03-02 22:00:49 -0500

    Add files via upload

--- easy_assign.py
+++ easy_assign.py
@@ -1,3 +1,5 @@
+import random
+
 '''
 Implement the following functions based on the question. Retain the name of the functions, and parameters as is in the question. 
 
@@ -21,3 +23,62 @@ Generate an input array and get an input for a number to search from the user. R
 
 
 '''
+
+def construct_matrix(m,n):
+    matrix=[]
+    for i in range(m):
+        temp_row=[]
+        for j in range(n):
+            temp_row.append(random.randint(1,50))
+        matrix.append(temp_row)
+    return matrix
+    
+
+n=6
+input_list=[]
+for i in range(n):
+        input_list.append(random.randint(1,1000))
+print(input_list)
+new_list=[]
+def return_below_n(input_list,value):
+    for i in range(n):
+        new_list.append(random.randint(1,value))
+    pass
+
+def search_for_item(input_list,item):
+    if item in input_list:
+        index=input_list.index(item)
+        return index
+    else:
+        return False
+    pass
+
+
+if __name__=="__main__":#construct matrix
+    m=int(input("How many Rows Should the Matrix Have?: "))
+    n=int(input("How many Columns Should the Matrix Have?: "))
+    matrix_1=construct_matrix(m,n)
+    print(matrix_1)
+    pass
+
+
+if __name__=="__main__":#return below n
+
+    value=int(input("Enter a Number for the List. The Second List Will be Smaller than this Number: "))
+    return_below_n(input_list,value)
+    print(new_list)
+
+
+
+if __name__=="__main__":# search for item
+    print(f"Here is a Randomly Generated List:{input_list}")
+    item=int(input("Enter a Number from the List: "))
+    
+    search_for_item(input_list,item)
+    if "index":
+        print(f"The Index is: {search_for_item(input_list,item)}.")
+    if item not in input_list:
+        print(-1)
+    
+
+
--- medium_assign.py
+++ medium_assign.py
@@ -22,3 +22,39 @@ To the function, send an input list such that it has duplicates. Return a new li
 BONUS: If the function has only one line -- +20%
 
 '''
+
+
+def string_max_length(strings_list):
+    longest_string=max(strings_list, key = len)
+    
+    return longest_string
+
+
+
+
+def matrix_multiplication(matrix_1,matrix_2):
+    #sorry but I have no clue how I would do this, I also don't even know the math clearly enough
+    pass
+
+
+
+def working_numbers_set(input_list):
+    return set(input_list)
+
+if __name__=="__main__":
+    list_1=["hello","grape","hi","world","outsider"]
+    print(f"The Original List is: {list_1}")
+    string_1=string_max_length(list_1)
+    print(f"The Longest String in the List is: {string_1}")
+    pass
+
+'''
+
+if __name__=="__main__":#working numbers set
+    list_1=[14,23,14,89,98,23,23,89,98]
+    print(f"List with Dupicates: {list_1}")
+    new_list=working_numbers_set(list_1)
+    print(f"List with Duplicates Removed: {new_list}")
+    pass
+
+'''
